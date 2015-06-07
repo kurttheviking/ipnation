834cbd5f7021260884435bbe8aa7416820ef5c30
Li Yi <liyi1234@gmail.com>
2015-06-07 07:54:52 -0400

    Fix throw in repl

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var async = require('bluebird').method;
 var sqlite3 = require('sqlite3');
 
 
-var db = new sqlite3.Database('ipnation.db');
+var db = new sqlite3.Database(__dirname + '/ipnation.db');
 var api = {
   fromIPv4: require('./lib/fromIPv4')
 };
